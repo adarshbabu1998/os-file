3db9f49f1ceac05bbbd9bcac58667861b4a8029a
adarsh <adarshbabualice@gmail.com>
2023-04-01 12:59:03 +0530

    commited

---
+++ osmodule.py
@@ -0,0 +1,18 @@
+import os 
+
+import shutil
+
+os.rename()
+os.remove('./demo.txt') 
+os.rmdir('new')
+
+shutil.rmtree('sample')
+x = './home.txt'
+y = 'filehandling'
+shutil.copy(x,y)
+
+m =  './poem.txt'
+n = 'filehandling'
+
+shutil.move(m,n)
+
